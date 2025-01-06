16173df4b7a7edda775ab8cca6779d81c15fbf6a
CEE <archmagece@users.noreply.github.com>
2025-01-06 11:06:11 +0900

    init - bundle gem sb-omniauth-kakao

---
+++ lib/sb/omniauth/kakao.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+require_relative "kakao/version"
+
+module Sb
+  module Omniauth
+    module Kakao
+      class Error < StandardError; end
+      # Your code goes here...
+    end
+  end
+end
---
+++ spec/sb/omniauth/kakao_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Sb::Omniauth::Kakao do
+  it "has a version number" do
+    expect(Sb::Omniauth::Kakao::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
